e0d14e2f0245f1596aa9d9e775793a1c090a43f1
Mario Antonioletti <mario@epcc.ed.ac.uk>
2025-07-28 09:26:54 +0100

    Take a new line on print.

--- src/mpi_hello.c
+++ src/mpi_hello.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
   /* All MPI Programs must call MPI_Init at the start */
   MPI_Init(&argc, &argv);
   
-  printf("Hello World!");
+  printf("Hello World!\n");
    
   /* Once all the MPI bits are finished you call MPI_Finalize */
   MPI_Finalize();
